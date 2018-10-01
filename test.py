c6ec96f8542638131c35f52d3ed96bd558bc93c5
Sen Heng <hengs@tcd.ie>
2018-10-01 15:53:42 +0100

    Sec commit

---
+++ test.py
@@ -0,0 +1,40 @@
+#print ("""FistName
+#MiddleName
+#LastName""")
+
+#print ("FistName\nMiddleName\nLastName")
+
+#input ("First Name is")
+#input ("Middle name is")
+#input ("Last name is")
+
+#print (float(input("Enter a number: ")) + float(input("Enter another number: ")))
+
+x = input ("x equal ")
+y = input ("y equal ")
+z = int(x) / int(y)
+print ("The divide result is:")
+print (int (z))
+
+#x=2
+#x +=3
+#print (x)
+
+#y = "python"
+#y += " is"
+#y += " fun"
+#print (y)
+
+#x = "football"
+#y = "python"
+#print ("I like %s" %y)
+#print ("I like %s and %s " %(x,y))
+
+
+#print (2==3)
+#print ('hello'=="hello")
+
+#print (2!=3)
+#print ('hello'!="hello")
+
+
