961a566055a6f4af7b733019051a43a54902f785
Sen Heng <hengs@tcd.ie>
2018-10-08 11:27:08 +0100

    commit homework-if

---
+++ homework-if.py
@@ -0,0 +1,27 @@
+
+money = int (input ("please input value between 10 and 50 or between 100 and 500 "))
+
+#if (money > 10 and money < 50) :
+#    print("value is > 10 and < 50")
+#if money > 100 and money < 500:
+#    print("value is > 100 and < 500")
+#elif (money > 50 and money < 100) or money > 500 or money < 10:
+#   print("value is out of the scope")
+
+#if money > 10 and money < 50 :
+#    print("value is > 10 and < 50")
+#elif money > 100 and money < 500:
+#    print("value is > 100 and < 500")
+#else:
+#    print("value is out of the scope")
+
+
+if money > 10 and money < 50 :
+    print("value is > 10 and < 50")
+else:
+    if money > 100 and money < 500:
+        print("value is > 100 and < 500")
+    else:
+        print("value is out of the scope")
+
+
